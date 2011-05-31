29eaf6df4ae600c20059639a158d51286bb431c1
mikoto20000 <mikoto20000@gmail.com>
2011-05-31 14:50:37 +0900

    DB作成が失敗するバグを修正.

--- config/createdb.sql
+++ config/createdb.sql
@@ -1,3 +1,3 @@
 create database railsgun character set utf8;
-create user 'railsgun'@'localhost' identified by 'railsgun';
-grant all privileges on railsgun.* to 'railsgun'@'localhost';
+create user 'railsgun' identified by 'railsgun';
+grant all privileges on railsgun.* to 'railsgun';
