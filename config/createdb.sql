c441a16e00685eefada5e9b5c706b2985e6218c4
mikoto20000 <mikoto20000@gmail.com>
2011-07-22 18:40:23 +0900

    データベースの認証がエラーになる不具合を修正。

--- config/createdb.sql
+++ config/createdb.sql
@@ -1,3 +1,4 @@
 create database railsgun character set utf8;
 create user 'railsgun' identified by 'railsgun';
-grant all privileges on railsgun.* to 'railsgun';
+grant all privileges on railsgun.* to railsgun@localhost IDENTIFIED by 'railsgun';
+
